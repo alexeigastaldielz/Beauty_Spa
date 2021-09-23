7115096553668e41a3a1334ddf0fb0cb25bf8253
R4f4S4lc3d0 <rafa22524@gmail.com>
2021-09-23 16:46:04 -0500

    Cambios chidos

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+(function() {
+  emailjs.init("user_nsfB75HsHb3gAeJ6RzBYc");
+  })();
 
-function sendMail(params){
-  var tempParams = {
-    name: document.getElementById("tonName").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("messages").value,
-  };
+function sendMail(){
+ 
+   let fullName = document.getElementById("name").value;
+   let userEmail = document.getElementById("email").value;
+   let userMessage = document.getElementById("message").value;
+  
+   var contactParams ={
+     from_name: fullName,
+     from_email: userEmail,
+     message : userMessage
+   };
 
-  emailjs.send("service_l8t7c6q","template_58vn7ck", tempParams)
-  .then (function(res){
-console.log("sucess", res.status);
-  })
+  emailjs.send("service_l8t7c6q","template_74px0yb", contactParams).then (function(res){})
 }
