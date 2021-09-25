1afbead547f7a5a216813342b5bef50ac5affe04
R4f4S4lc3d0 <rafa22524@gmail.com>
2021-09-24 23:25:05 -0500

    Cambios prrones

--- Carpetajs/contacto .js
+++ Carpetajs/contacto .js
@@ -1,22 +1,29 @@
 //Código para enviar correos
 
-(function() {
+(function () {
   emailjs.init("user_nsfB75HsHb3gAeJ6RzBYc");
-  })();
+})();
 
-function sendMail(){
- 
-   let fullName = document.getElementById("name").value;
-   let userEmail = document.getElementById("email").value;
-   let userMessage = document.getElementById("message").value;
-  
-   var contactParams ={
-     from_name: fullName,
-     to_email: userEmail,
-     message : userMessage
-   };
+function sendMail() {
+  let fullName = document.getElementById("name").value;
+  let userEmail = document.getElementById("email").value;
+  let userMessage = document.getElementById("message").value;
 
-  emailjs.send("service_l8t7c6q","template_74px0yb", contactParams).then (function(res){})
+  var contactParams = {
+    from_name: fullName,
+    from_email: userEmail,
+    message: userMessage,
+  };
+
+  emailjs
+    .send("service_l8t7c6q", "template_74px0yb", contactParams)
+    .then(function (res) {});
+
+  return true;
+}
+
+function envi() {
+  window.location.reload();
 }
 
 //Código para la NavBar
